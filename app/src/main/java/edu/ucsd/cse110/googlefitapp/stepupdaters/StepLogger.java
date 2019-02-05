30ff5c2f96b2fb03d7401a44c467b74814227448
KevinDly <46756246+KevinDly@users.noreply.github.com>
2019-02-05 11:02:36 -0800

    StepLogger updated.

--- app/src/main/java/edu/ucsd/cse110/googlefitapp/stepupdaters/StepLogger.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/stepupdaters/StepLogger.java
@@ -1,13 +1,37 @@
 package edu.ucsd.cse110.googlefitapp.stepupdaters;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.preference.Preference;
 
-public class StepLogger {
-    //
-    /*
-    Preference
-    StepLogger( ) {
+import static android.content.Context.*;
 
+public class StepLogger {
+
+    SharedPreferences sharedPref;
+
+    StepLogger(Context context){
+        sharedPref = context.getSharedPreferences("step_data", MODE_PRIVATE);
     }
-    */
+
+    /*Writes all step data to logger*/
+    public void writeSteps(int dailySteps, int totalSteps, int lastStepUpdate){
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putInt("daily_steps", dailySteps);
+        editor.putInt("total_steps", totalSteps);
+        editor.putInt("last_update", lastStepUpdate);
+        editor.apply();
+    }
+
+    public int readDaily(){
+        return sharedPref.getInt("daily_steps", 0);
+    }
+
+    public int readTotal(){
+        return sharedPref.getInt("total_steps", 0);
+    }
+
+    public int readLastStep(){
+        return sharedPref.getInt("last_update", 0);
+    }
 }
