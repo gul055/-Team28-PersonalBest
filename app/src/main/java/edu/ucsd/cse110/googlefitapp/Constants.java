1623f796366533d4b935334404064029a1726296
Daiki Masuda <arrestrose@gmail.com>
2019-02-13 12:59:51 -0800

    Basic code for rendering a graph

--- app/src/main/java/edu/ucsd/cse110/googlefitapp/Constants.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/Constants.java
@@ -1,14 +1,22 @@
 package edu.ucsd.cse110.googlefitapp;
 
 public final class Constants {
+
+    // Goal constants
     public static final int MINIMUM_VALID_GOAL = 100;
     public static final String GOAL = "goal";
     public static final String SET_SUCCESS = "New goal set!";
     public static final String SET_FAIL = "Invalid goal. Please try again.";
     public static final String NO_GOAL = "No goal inputted!";
+
+    // Walk/run constants
     public static final String START_WALK = "Start Walk/Run";
     public static final String STOP_WALK = "Stop Walk/Run";
 
     // Specific requestor code for setting goal
     public static final int GOAL_ACTIVITY_CODE = 1;
+
+    // Graph constants
+    public static final int BAR_WIDTH = 1;
+    public static final boolean FIT_BARS = true;
 }
--- app/src/main/java/edu/ucsd/cse110/googlefitapp/DataHandler.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/DataHandler.java
@@ -1,6 +1,7 @@
 package edu.ucsd.cse110.googlefitapp;
 
-public class StepsGraph {
+public class DataHandler {
 
     // https://github.com/PhilJay/MPAndroidChart/wiki/Getting-Started
+    // Need to return an array of floats
 }
