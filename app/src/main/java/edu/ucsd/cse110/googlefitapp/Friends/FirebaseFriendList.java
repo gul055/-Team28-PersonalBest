d99ffcc89eb6aa55ddfb0cdce13a01144d38e301
Gretal Dea <deagretal@gmail.com>
2019-03-09 15:17:02 -0800

    #64 can add friends now!!

--- app/src/main/java/edu/ucsd/cse110/googlefitapp/Friends/FirebaseFriendList.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/Friends/FirebaseFriendList.java
@@ -1,7 +1,9 @@
 package edu.ucsd.cse110.googlefitapp.Friends;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,14 +27,18 @@ public class FirebaseFriendList implements IFriendList, ISubject<IFriendObserver
     CollectionReference db;
     String myEmail;
     private Collection<IFriendObserver> observers;
+    String result = "";
+    Context context;
 
-    public FirebaseFriendList(String email) {
+    public FirebaseFriendList(Context context, String email) {
         db = FirebaseFirestore.getInstance()
                 .collection(FRIEND_COLLECTION_KEY);
 
         myEmail = email;
 
         observers = new ArrayList<>();
+
+        this.context = context;
     }
 
     @Override
@@ -46,72 +52,85 @@ public class FirebaseFriendList implements IFriendList, ISubject<IFriendObserver
         //put B in A's friend list
         //if not, check if B is already in A's friend list -> do nothing
         //otherwise, add A to B's pending list
-        Task<QuerySnapshot> userData = db.get();
+        //return message
+        db.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                //user as hashmap object
+                Map<String, Object> currentUser = new HashMap<>();
+                Map<String, Object> otherUser = new HashMap<>();
 
-        //user as hashmap object
-        Map<String, Object> currentUser = (Map<String, Object>) db.whereEqualTo("user", myEmail).get();
-        Map<String, Object> otherUser = (Map<String, Object>) db.whereEqualTo("user", email).get();
+                //find current user and other user's document
+                for(QueryDocumentSnapshot doc : task.getResult()) {
+                    Map<String, Object> curr = doc.getData();
+                    String currEmail = (String) curr.get("user");
 
-        //document id for updating data
-        String currId = "";
-        String otherId = "";
+                    if(currEmail.equals(myEmail)) {
+                        currentUser = curr;
+                    }
+                    else if(currEmail.equals(email)) {
+                        otherUser = curr;
+                    }
+                }
 
+                //if users are not found, don't do anything
+                if(otherUser.size() == 0) {
+                    Log.d(Constants.FRIEND_TAG, "user does not exist");
+                    result = "Could not find user " + email;
+                    Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+                    return;
+                }
 
-        //find current user's document and other user's document
-        for(QueryDocumentSnapshot doc : userData.getResult()) {
-            Map<String, Object> curr = doc.getData();
-            String currEmail = (String) curr.get("user");
+                //get users' friend and pending lists
+                List<String> otherPending = (List<String>) otherUser.get("pending");
+                List<String> otherFriends = (List<String>) otherUser.get("friends");
+                List<String> myPending = (List<String>) currentUser.get("pending");
+                List<String> myFriends = (List<String>) currentUser.get("friends");
 
-            if(currEmail.equals(myEmail)) {
-                currId = doc.getId();
-                currentUser = curr;
+                //users are already friends with each other
+                if(otherFriends.contains(myEmail) && myFriends.contains(email)) {
+                    Log.d(Constants.FRIEND_TAG, "already friends with this user");
+                    result = "You are already friends with " + email;
+                    Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                //if other user is in current user's pending list, they become friends
+                else if(myPending.contains(email)) {
+                    Log.d(Constants.FRIEND_TAG, "becoming friends");
+                    myPending.remove(email);
+                    myFriends.add(email);
+                    otherFriends.add(myEmail);
+
+                    result = "Added " + email;
+                    Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+
+                    //notify listeners
+                    for(IFriendObserver observer : observers) {
+                        observer.onStateChange(email);
+                    }
+                }
+                //put this user in other user's pending friends
+                else {
+                    Log.d(Constants.FRIEND_TAG, "putting user in pending");
+                    if(!otherPending.contains(myEmail)) {
+                        result = "You will be friends with " + email + " when they add you as a friend.";
+                        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+                        otherPending.add(myEmail);
+                    }
+                    else {
+                        result = "Already added! You will be friends with " + email + " when they add you as a friend.";
+                        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+
+                    }
+                }
+
+                //make the changes in the database
+                updateDatabase(myEmail, myFriends, myPending);
+                updateDatabase(email, otherFriends, otherPending);
             }
-            else if(currEmail.equals(email)) {
-                otherId = doc.getId();
-                otherUser = curr;
-            }
-        }
+        });
 
-        //if users are not found, don't do anything
-        if(currentUser.size() == 0 || otherUser.size() == 0) {
-            Log.d(Constants.FRIEND_TAG, "user does not exist");
-            return;
-        }
-
-        //get users' friend and pending lists
-        List<String> otherPending = (List<String>) otherUser.get("pending");
-        List<String> otherFriends = (List<String>) otherUser.get("friends");
-        List<String> myPending = (List<String>) currentUser.get("pending");
-        List<String> myFriends = (List<String>) currentUser.get("friends");
-
-        //users are already friends with each other
-        if(otherFriends.contains(myEmail) && myFriends.contains(email)) {
-            Log.d(Constants.FRIEND_TAG, "already friends with this user");
-            return;
-        }
-        //if other user is in current user's pending list, they become friends
-        else if(myPending.contains(email)) {
-            Log.d(Constants.FRIEND_TAG, "becoming friends");
-            myPending.remove(email);
-            myFriends.add(email);
-            otherFriends.add(myEmail);
-
-            //notify listeners
-            for(IFriendObserver observer : observers) {
-                observer.onStateChange(email);
-            }
-        }
-        //put this user in other user's pending friends
-        else {
-            Log.d(Constants.FRIEND_TAG, "putting user in pending");
-            if(!otherPending.contains(myEmail)) {
-                otherPending.add(myEmail);
-            }
-        }
-
-        //make the changes in the database
-        updateDatabase(myEmail, myFriends, myPending);
-        updateDatabase(email, otherFriends, otherPending);
+        Log.d(Constants.FRIEND_TAG, "end add friend process");
     }
 
     private void updateDatabase(String email, List<String> friends, List<String> pending) {
@@ -120,6 +139,7 @@ public class FirebaseFriendList implements IFriendList, ISubject<IFriendObserver
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
                     for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(Constants.FRIEND_TAG, "updating user " + email);
                         Map<String, Object> map = new HashMap<>();
                         map.put("user", email);
                         map.put("friends", friends);
@@ -133,29 +153,32 @@ public class FirebaseFriendList implements IFriendList, ISubject<IFriendObserver
 
     @Override
     public void loadFriends() {
-        Task<QuerySnapshot> userData = db.get();
+        db.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                Map<String, Object> currentUser = new HashMap<String, Object>();
 
-        Map<String, Object> currentUser = new HashMap<String, Object>();
+                //find current user's document
+                for(QueryDocumentSnapshot doc : task.getResult()) {
+                    Map<String, Object> curr = doc.getData();
+                    String currEmail = (String) curr.get("user");
 
-        //find current user's document
-        for(QueryDocumentSnapshot doc : userData.getResult()) {
-            Map<String, Object> curr = doc.getData();
-            String currEmail = (String) curr.get("user");
+                    if(currEmail.equals(myEmail)) {
+                        currentUser = curr;
+                    }
+                }
 
-            if(currEmail.equals(myEmail)) {
-                currentUser = curr;
+                //get current user's friend list
+                List<String> myFriends = (List<String>) currentUser.get("friends");
+
+                //notify observers of each friend
+                for(String email : myFriends) {
+                    for (IFriendObserver observer : observers) {
+                        observer.onStateChange(email);
+                    }
+                }
             }
-        }
-
-        //get current user's friend list
-        List<String> myFriends = (List<String>) currentUser.get("friends");
-
-        //notify observers of each friend
-        for(String email : myFriends) {
-            for (IFriendObserver observer : observers) {
-                observer.onStateChange(email);
-            }
-        }
+        });
     }
 
     @Override
