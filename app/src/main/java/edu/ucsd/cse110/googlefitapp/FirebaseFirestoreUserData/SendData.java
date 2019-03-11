2d27151db09f9363aae899c9573bb0370d759e47
Daiki Masuda <arrestrose@gmail.com>
2019-03-10 19:23:16 -0700

    Changing to accept email

--- app/src/main/java/edu/ucsd/cse110/googlefitapp/FirebaseFirestoreUserData/SendData.java
+++ app/src/main/java/edu/ucsd/cse110/googlefitapp/FirebaseFirestoreUserData/SendData.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.ucsd.cse110.googlefitapp.Utils.GoogleUserUtil;
 import edu.ucsd.cse110.googlefitapp.Utils.SharedPreferencesUtil;
 
 public class SendData {
@@ -14,7 +15,7 @@ public class SendData {
 
     public SendData(Context c){
         context = c;
-        adapter = StepDataAdapter.getInstance(c);
+        adapter = StepDataAdapter.getInstance(GoogleUserUtil.getEmail(c));
     }
 
     // Given the key for SharedPreferences, sends user's step data to Firebase in the form of a long
